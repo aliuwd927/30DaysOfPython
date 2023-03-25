dff8d9e47c9ca618b7e45de77c1da4f67417f323
Andy Liu <aliu927@gmail.com>
2023-03-24 22:42:31 -0700

    Finished Exercise 1 and 2

---
+++ day1.py
@@ -0,0 +1,19 @@
+print(3+4)  # Addition
+print(3-4)  # Subtraction
+print(3*4)  # Mulitiplication
+print(3/4)  # Division
+print(3 % 4)  # Modulus
+print(3**4)  # Exponential
+print(3//3)  # Floor Division
+
+print("John Doe")
+print("The Does")
+print("USA")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["One", "Two", "Three"]))
+print(type('Johne Doe'))
